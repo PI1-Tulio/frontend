2f39d9ba05fefbccaeaf4dd8a28b0ff581aa728a
felipeJRdev <felipe123rodrigues1@gmail.com>
2025-10-30 00:14:24 -0300

    chore: configuracoes iniciais

---
+++ src/pages/instrucao/Instrucao.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function Instrucao() { 
+  return (
+    <div>
+      <h1>Página Inicial (Instrucao)</h1>
+      <p>TEste!</p>
+    </div>
+  );
+}
+
+export default Instrucao;
