5e763875123586c60dc564ab9a275ac4caeefc85
guga642 <gustavoaugusto0803@gmail.com>
2025-11-06 17:23:57 -0300

    feat: add clean header component with battery and time indicators

---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Header.css';
+
+interface HeaderProps {
+  batteryPercentage?: number;
+  timeRemaining?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ 
+  batteryPercentage = 97, 
+  timeRemaining = '57m restantes' 
+}) => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <div className="left-section">
+          {/* Texto removido - deixando vazio para manter a estrutura */}
+        </div>
+        
+        <div className="right-section">
+          <div className="battery-info">
+            <div className="battery-container">
+              <div 
+                className="battery-level"
+                style={{ width: `${batteryPercentage}%` }}
+              ></div>
+            </div>
+            <span className="battery-percentage">{batteryPercentage}%</span>
+          </div>
+          
+          <div className="time-info">
+            <span className="time-remaining">{timeRemaining}</span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
